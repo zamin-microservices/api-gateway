0900ae6faf95188a0e0045d46211118914ec0648
zamss <aqeel.zamin@gmail.com>
2023-11-16 07:24:41 +0530

    first commit

---
+++ src/test/java/com/zamin/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamin.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
